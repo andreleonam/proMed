3493cfa7da6b7351fb125d5e9aaa739fec501471
Andre Leonam <AndreLeonam@icloud.com>
2019-10-18 17:24:07 -0300

    Projeto criado

---
+++ src/test/java/com/tripod/promed/PromedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripod.promed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
