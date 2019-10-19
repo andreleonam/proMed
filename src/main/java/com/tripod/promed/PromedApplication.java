71f1a8cf18eb5c472ee16cc0d65531b8a498961a
Andre Leonam Costa Silva Santana <andreleonam@icloud.com>
2019-10-19 03:21:17 -0300

    Criando operacao de instanciacao

--- src/main/java/com/tripod/promed/PromedApplication.java
+++ src/main/java/com/tripod/promed/PromedApplication.java
@@ -1,13 +1,33 @@
 package com.tripod.promed;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class PromedApplication {
+import com.tripod.promed.domain.Exam;
+import com.tripod.promed.repositories.ExamRepository;
 
+@SpringBootApplication
+public class PromedApplication implements CommandLineRunner {
+
+	@Autowired
+	private ExamRepository examRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PromedApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Exam ex1 = new Exam(null, "Hemograma", "HEM");
+		Exam ex2 = new Exam(null, "Albumina", "ALB");
+		Exam ex3 = new Exam(null, "Amilase", "AMI");
+	
+		examRepository.saveAll(Arrays.asList(ex1, ex2, ex3));
 	}
 
 }
