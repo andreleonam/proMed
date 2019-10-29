587044ed1ea53b546d921b4c8f44aae145312dd2
Andre Leonam Costa Silva Santana <andreleonam@icloud.com>
2019-10-28 21:02:27 -0300

    Correcao classe prescription services

--- src/main/java/com/tripod/promed/services/PrescriptionService.java
+++ src/main/java/com/tripod/promed/services/PrescriptionService.java
@@ -3,10 +3,12 @@ package com.tripod.promed.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.tripod.promed.domain.Prescription;
 import com.tripod.promed.repositories.PrescriptionRepository;
 
+@Service
 public class PrescriptionService {
 
 	@Autowired
